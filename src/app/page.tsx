2e85e6a1f5aaca0104644160dc545d44024499ed
Reece Buyan <54456838+rpbuyan@users.noreply.github.com>
2024-06-22 18:00:14 -0700

    Updated changes for main page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Roomspage from "./rooms";
 export default function Home() {
   return (
     <main>
-      <div className=" p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+      <div className=" p-4 flex h-7 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
         <span className="font-bold">EngageEye</span>
         <ModeToggle />
       </div>
--- src/app/rooms.tsx
+++ src/app/rooms.tsx
@@ -3,11 +3,14 @@
 import { Textarea } from "@/components/ui/textarea";
 import CameraView from "./cameraView";
 import { Button } from "@/components/ui/button";
+import PhoneIcon from "@/components/ui/phoneIcon"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+
 
 export default function Roomspage() {
   return (
     <div className="grid grid-cols-4 min-h-screen">
-      <div className="col-span-3 p-4 pr-2">
+      <div className="col-span-2 p-4 pr-2">
         <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 min-h-screen flex items-start justify-center">
           <CameraView />
         </div>
@@ -18,6 +21,30 @@ export default function Roomspage() {
           <h1 className="text-base">Topic</h1>
           <Textarea placeholder="Input your prompt for EVI" className="text-base text-gray-600"/>
           <Button variant="default" className="w-full">Submit</Button>
+        </div>
+      </div>
+
+      <div className="col-span-1 p-4 pl-2 flex flex-col">
+        <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 flex flex-col gap-4">
+          <h1 className="text-base">Insights Report</h1>
+          
+          <Alert>
+            <PhoneIcon className="h-4 w-4" />
+            <AlertTitle>Heads up!</AlertTitle>
+            <AlertDescription>
+              Make sure to call this phone number in order to debate! *INSERT PHONE NUM HERE*
+            </AlertDescription>
+          </Alert>
+
+          <Alert>
+            <PhoneIcon className="h-4 w-4" />
+            <AlertTitle>Another reminder!</AlertTitle>
+            <AlertDescription>
+              Have fun and use this AI responsibly!
+            </AlertDescription>
+          </Alert>
+          {/* <Textarea placeholder="Input your prompt for EVI" className="text-base text-gray-600"/> */}
+          {/* <Button variant="default" className="w-full">Submit</Button> */}
         </div>
       </div>
     </div>
