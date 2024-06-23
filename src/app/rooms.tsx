45c45f78074ea81fa9b698a3742199cb84c8ffbd
Aakash M <mr.maakash@gmail.com>
2024-06-22 23:39:39 -0700

    camera formatting

--- src/app/rooms.tsx
+++ src/app/rooms.tsx
@@ -3,27 +3,28 @@
 import { Textarea } from "@/components/ui/textarea";
 import CameraView from "./cameraView";
 import { Button } from "@/components/ui/button";
-import PhoneIcon from "@/components/ui/phoneIcon"
-
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-
+import PhoneIcon from "@/components/ui/phoneIcon";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function Roomspage() {
   return (
     <div className="grid grid-cols-3 min-h-screen">
-      <div className="col-span-2 p-4 pr-2">
-        <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 min-h-screen flex items-start justify-center">
-          <CameraView />
+      <div className="col-span-3 md:col-span-2 p-4 pr-2">
+        <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-1 min-h-screen flex-col justify-center">
+          <div className="flex items-center justify-center w-full">
+            <CameraView />
+          </div>
+          <div className="w-full mt-2">
+            <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-2 flex flex-col gap-4">
+              <h1 className="text-base">Topic</h1>
+              <Textarea placeholder="Input your debate topic for Cicero" className="text-base text-gray-600" />
+              <Button variant="default" className="w-full">Submit</Button>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="col-span-1 p-4 pl-2 flex flex-col">
-        <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 flex flex-col gap-4">
-          <h1 className="text-base">Topic</h1>
-          <Textarea placeholder="Input your debate topic for Cicero" className="text-base text-gray-600"/>
-          <Button variant="default" className="w-full">Submit</Button>
-        </div>
-
+      <div className="col-span-3 md:col-span-1 p-4 pl-2 flex flex-col">
         <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 flex flex-col gap-4">
           <h1 className="text-base">Insights Report</h1>
           
@@ -46,7 +47,6 @@ export default function Roomspage() {
           <Button variant="default" className="w-full">Restart</Button>
         </div>
       </div>
-        
     </div>
   );
 }
