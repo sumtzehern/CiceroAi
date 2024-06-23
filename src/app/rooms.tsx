b1c031409c39ec0de50cff93b6a46705a605e8c2
Aakash M <mr.maakash@gmail.com>
2024-06-22 22:46:53 -0700

    fixes

--- src/app/rooms.tsx
+++ src/app/rooms.tsx
@@ -20,7 +20,7 @@ export default function Roomspage() {
       <div className="col-span-1 p-4 pl-2 flex flex-col">
         <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 flex flex-col gap-4">
           <h1 className="text-base">Topic</h1>
-          <Textarea placeholder="Input your prompt for EVI" className="text-base text-gray-600"/>
+          <Textarea placeholder="Input your debate topic for Cicero" className="text-base text-gray-600"/>
           <Button variant="default" className="w-full">Submit</Button>
         </div>
 
